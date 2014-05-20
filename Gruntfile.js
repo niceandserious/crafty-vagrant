b77b64af320d8382765daeb34b24c42b15f3ae2a
Nick Fraenkel <nick.f@niceandserious.com>
2014-05-20 16:02:54 +0100

    Grunt: added path to normalize-scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,8 @@ module.exports = function(grunt) {
 					// Allow easy importing of bourbon and neat (via eg. @import "bourbon"):
 					loadPath: [
 						'app/public/bower_components/bourbon/app/assets/stylesheets',
-						'app/public/bower_components/neat/app/assets/stylesheets'
+						'app/public/bower_components/neat/app/assets/stylesheets',
+						'app/public/bower_components/normalize-scss'
 					]
 				},
 				files: {
