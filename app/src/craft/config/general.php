01280e1f373301186fd39f5b59412fb125de5e73
Nick Fraenkel <nick.f@niceandserious.com>
2014-08-27 20:29:54 +0100

    Crafty/Config: added default general config (set .dev to dev mode)

---
+++ app/src/craft/config/general.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+	'*' => array(
+		'devMode' => false,
+	),
+	'.dev' => array(
+		'devMode' => true,
+	),
+);
