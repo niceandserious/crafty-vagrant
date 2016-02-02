79b9d97789b255a39d467d7d67b3ef4ecf2fb096
Nick Fraenkel <nick.f@niceandserious.com>
2016-02-02 11:57:10 +0000

    Install.sh: tweaked whitespace (indent with spaces)

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@
 ##
 
 ## Colours for prettier output + to distinguish this script's output:
-color='\033[1;36m';		# Light cyan
-NC='\033[0m';			# No color
+color='\033[1;36m';   # Light cyan
+NC='\033[0m';     # No color
 
 ## Function to prompt for user response:
 prompt () {
     while true; do
-    	echo;
+      echo;
         read -p "$1" yn
         case $yn in
             [Yy]* ) return 0;;
@@ -24,12 +24,12 @@ prompt () {
 
 ## output in colour:
 echo_color() {
-	echo -e "${color}$1${NC}";
+  echo -e "${color}$1${NC}";
 }
 
 if ! prompt "Do you wish to install Crafty Vagrant? [yn] "; then
-	echo_color 'Ok. Install cancelled.';
-	exit;
+  echo_color 'Ok. Install cancelled.';
+  exit;
 fi
 
 ## If Craft isn't present:
@@ -58,23 +58,23 @@ echo_color "
 ## Setting up Craft...";
 
 ## Activate the htaccess (rename 'htaccess' --> '.htaccess'):
-if [ -f "app/public/htaccess" ]; then 
-	echo "## Activating htaccess...";
-	mv app/public/htaccess app/public/.htaccess;
+if [ -f "app/public/htaccess" ]; then
+  echo "## Activating htaccess...";
+  mv app/public/htaccess app/public/.htaccess;
 fi
 
 ## Replace the default templates + config:
 CRAFT_SRC="app/src/craft";
 
 if [ -d $CRAFT_SRC ] && prompt "Replace Craft's default templates + config? [yn] "; then
-	echo "Replacing Craft files... (copying from ${CRAFT_SRC})";
-	
-	## Remove existing remplates, if present:
-	[ -d "app/craft/templates" ] && rm -r app/craft/templates;
+  echo "Replacing Craft files... (copying from ${CRAFT_SRC})";
 
-	## Copy everything from app/src/craft to the app/craft directory,
-	## overwriting existing files:
-	cp -R $CRAFT_SRC app;
+  ## Remove existing remplates, if present:
+  [ -d "app/craft/templates" ] && rm -r app/craft/templates;
+
+  ## Copy everything from app/src/craft to the app/craft directory,
+  ## overwriting existing files:
+  cp -R $CRAFT_SRC app;
 fi
 
 ## Run Grunt, to render CSS / Javascript / etc:
@@ -86,9 +86,9 @@ grunt;
 ## (to prevent a PHP error on first visit to craft.dev)
 DIR="app/craft/storage/runtime";
 if [ ! -f $DIR/.gitignore ]; then
-	mkdir -p $DIR/{cache,compiled_templates,logs};
-	touch "$DIR/.gitignore";
-	touch "$DIR/logs/craft.log";
+  mkdir -p $DIR/{cache,compiled_templates,logs};
+  touch "$DIR/.gitignore";
+  touch "$DIR/logs/craft.log";
 fi
 
 echo_color "
