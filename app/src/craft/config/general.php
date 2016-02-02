591d2666d638c2c83a85973c4159036019cd4114
Nick Fraenkel <nick.f@niceandserious.com>
2016-02-02 12:58:13 +0000

    Config: define siteUrl for all environments
    (Also, whitespace changed from tabs to spaces)

--- app/src/craft/config/general.php
+++ app/src/craft/config/general.php
@@ -7,12 +7,20 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
+// Define some constants that let us set up some environment variables:
+// (eg. sitePath)
+define('URI_SCHEME', (isset($_SERVER['HTTPS'] ) ) ? "https://" : "http://");
+define('SITE_URL',    URI_SCHEME.$_SERVER['SERVER_NAME'].'/');
+
 return array(
-	'*' => array(
-		'devMode' => false,
-		'omitScriptNameInUrls' => true,
-	),
-	'.dev' => array(
-		'devMode' => true,
-	),
+  '*' => array(
+    'devMode' => false,
+    'omitScriptNameInUrls' => true,
+    'environmentVariables' => array(
+      'siteUrl'  => SITE_URL
+    ),
+  ),
+  '.dev' => array(
+    'devMode' => true,
+  ),
 );
