838ba4176b6629a4c17b8f785ec3881ddee58326
Nick Fraenkel <nick.f@niceandserious.com>
2016-06-15 13:21:16 +0100

    Puppet/DB-setup: made message slightly more friendly / helpful, plus normalised whitespace

--- puppet/db-setup.sh
+++ puppet/db-setup.sh
@@ -4,20 +4,21 @@
 
 ## Check whether there's a database backup:
 if [ -d "/vagrant/app/craft/storage/backups" ]; then
-	db_init=`ls /vagrant/app/craft/storage/backups | tail -1`;
+  db_init=`ls /vagrant/app/craft/storage/backups | tail -1`;
 else
-	db_init='';
+  db_init='';
 fi
 
 ## If there is, restore it:
 if test ! -s $db_init
 then
-	echo
-	echo "Populating Craft database from latest backup ($db_init)";
-	mysql -u$1 -p$2 $3 < /vagrant/app/craft/storage/backups/$db_init;
-	echo
+  echo
+  echo "Populating Craft database from latest backup ($db_init)";
+  mysql -u$1 -p$2 $3 < /vagrant/app/craft/storage/backups/$db_init;
+  echo
 else
-	echo
-	echo 'No Craft database backups found. Please install Craft manually.';
-	echo
+  echo
+  echo "No Craft database backups found.";
+  echo "Please finish installing Craft by visiting craft.dev/admin/install in your browser.";
+  echo
 fi
