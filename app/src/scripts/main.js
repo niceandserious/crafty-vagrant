1f45554deb8c5e85a23542f571319dc395e920cd
Marcel Legane <marcel@niceandserious.com>
2017-03-03 14:55:34 +0000

    Update craft templates and base css/js

--- app/src/scripts/main.js
+++ app/src/scripts/main.js
@@ -1,12 +1,2 @@
-'use strict';
-
-// Import Javascript modules here:
-var example = require('./modules/example');
-
-// ...and then do something with them:
-example.init();
-
-// This all gets processed by Babel, so feel free
-// to use ES2015 syntax if you like - it will be
-// transpiled to backwards-compatible ES5:
-let b = [1,2,3].map(x => x * 2);
+import $ from 'jquery';
+window.$ = $;
